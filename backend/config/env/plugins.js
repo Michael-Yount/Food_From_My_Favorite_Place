6bbab0be411c016a9af644a0b4d8f88517c365f8
Michaeal Yount <cheshirekatsmile101@gmail.com>
2023-09-11 15:50:05 -0400

    plugins

--- backend/config/env/plugins.js
+++ backend/config/env/plugins.js
@@ -1,15 +1,31 @@
-module.exports = {
-    graphql: {
-      config: {
-        endpoint: "/graphql",
-        shadowCRUD: true,
-        playgroundAlways: true,
-        depthLimit: 10,
-        amountLimit: 100,
-        apolloServer: {
-          tracing: false,
-          introspection: true,
-        },
+module.exports = ({ env }) => ({
+  // ...
+  upload: {
+    config: {
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
       },
     },
-  };
+  },
+  graphql: {
+    config: {
+      endpoint: "/graphql",
+      shadowCRUD: true,
+      playgroundAlways: true,
+      depthLimit: 10,
+      amountLimit: 100,
+      apolloServer: {
+        tracing: false,
+        introspection: true,
+      },
+    },
+  },
+});
