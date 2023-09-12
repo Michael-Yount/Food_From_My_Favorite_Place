d81fe06ab09054b23eed01e23c06c7333601fc7a
Michaeal Yount <cheshirekatsmile101@gmail.com>
2023-09-12 04:12:33 -0400

    localhost

--- frontend/pages/restaurant/[id].js
+++ frontend/pages/restaurant/[id].js
@@ -55,7 +55,7 @@ function DishCard({ data }) {
           className="w-full rounded-2xl"
           height={300}
           width={300}
-          src={`${process.env.STRAPI_URL || ""}${
+          src={`${process.env.STRAPI_URL || "http://127.0.0.1:1337"}${
             data.attributes.image.data.attributes.url
           }`}
           alt="dish image"
