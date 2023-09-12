9888fd34ad1740b169bf267dccdaa972cba20817
Michaeal Yount <cheshirekatsmile101@gmail.com>
2023-09-12 11:45:19 -0400

    fix env?

--- frontend/components/RestaurantList.js
+++ frontend/components/RestaurantList.js
@@ -33,7 +33,7 @@ function RestaurantCard({ data }) {
           height={300}
           width={300}
           src={`${process.env.STRAPI_URL || ""}${
-            data.attributes.image.data[0].attributes.url
+            data.attributes.image.data.attributes.url
           }`}
           alt="Restaurant Photo"
         />
